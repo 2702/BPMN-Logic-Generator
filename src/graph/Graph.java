7bd1fcf29b50d6e2df614d95e489611633fbe80a
Maciej Czekanski <maves90@gmail.com>
2012-06-06 01:07:11 +0200

    Changed id type to String

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -5,34 +5,35 @@ import java.util.ArrayList;
 //graf skierowany. nie przechowuje krawedzi
 public class Graph {
 	ArrayList<Node> nodes;
-	
-	public Graph(){
+
+	public Graph() {
 		nodes = new ArrayList<Node>();
 	}
-	
-	//TODO powinien rzucac wyjatek 
-	public Node findNodeById(int id){
+
+	// TODO powinien rzucac wyjatek
+	public Node findNodeById(String id) {
 		Node foundNode = null;
-		for (Node node: nodes){
-			if (node.id == id){
+		for (Node node : nodes) {
+			if (node.getId().equals(id)) {
 				foundNode = node;
 			}
 		}
 		return foundNode;
 	}
-	
-	//TODO wyjatek przy istniejacym id
-	public void addNode(int id, String formula){
+
+	// TODO wyjatek przy istniejacym id
+	public void addNode(String id, String formula) {
 		Node node = new Node(id, formula);
 		nodes.add(node);
 	}
-	
+
 	// polaczenie niesymetryczne
-	public void addConnection(int idFrom, int idWhere){
-		this.findNodeById(idFrom).connectWithOtherNode(this.findNodeById(idWhere));
+	public void addConnection(String idFrom, String idWhere) {
+		this.findNodeById(idFrom).connectWithOtherNode(
+				this.findNodeById(idWhere));
 	}
-	
-	public String toString(){
+
+	public String toString() {
 		return nodes.toString();
 	}
 
@@ -43,18 +44,17 @@ public class Graph {
 	public void setNodes(ArrayList<Node> nodes) {
 		this.nodes = nodes;
 	}
-	
-	public void removeNode(Node node){
+
+	public void removeNode(Node node) {
 		nodes.remove(node);
 	}
-	
-	public void addNode(Node node){
+
+	public void addNode(Node node) {
 		nodes.add(node);
 	}
-	
-	
-	public void cleanupEdges(){
-		for (Node node : nodes){
+
+	public void cleanupEdges() {
+		for (Node node : nodes) {
 			node.cleanupEdges();
 		}
 	}
